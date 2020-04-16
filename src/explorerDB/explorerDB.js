917749257cc18a5f815b3ca5cd03d2fa8cf19d5e
AntonioAlejandro01 <antonioalex98@live.com>
2020-04-17 00:06:01 +0200

    update .gitignore and credentials db

--- src/explorerDB/explorerDB.js
+++ src/explorerDB/explorerDB.js
@@ -2,9 +2,9 @@ import mysql from 'mysql';
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'explorer',
-    password: 'Explorer2@2@Node',
-    database: 'Explorer',
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWD,
+    database: process.env.DBdb,
     port: 3306,
 });
 
